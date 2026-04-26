24b8141dd8a05d00bf1ffe500f8cd7dae2cdfb43
Yuri Gossmann <yuripg1@gmail.com>
2026-04-26 00:33:15 -0300

    feat: updated to use DeepSeek v4 models

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import random
 import requests
 from rich import console as rich_console, markdown as rich_markdown
+import time
 import typing
 import subprocess
 import json
 
 FunctionType = typing.Literal["function"]
-ModelType = typing.Literal["deepseek-chat", "deepseek-reasoner"]
 RoleType = typing.Literal["assistant", "tool", "user", "system"]
 ToolChoiceType = typing.Literal["auto"]
+DeepSeekModelType = typing.Literal["deepseek-v4-flash", "deepseek-v4-pro"]
+DeepSeekThinkingType = typing.Literal["enabled", "disabled"]
+DeepSeekReasoningEffortType = typing.Literal["high", "max"]
 
 
 class ToolCallFunction(typing.TypedDict):
@@ -21,7 +25,7 @@ class ToolCall(typing.TypedDict):
     function: typing.Required[ToolCallFunction]
 
 
-class DeepSeekMessage(typing.TypedDict):
+class DeepSeekRequestMessage(typing.TypedDict):
     role: typing.Required[RoleType]
     content: typing.NotRequired[str]
     reasoning_content: typing.NotRequired[str]
@@ -29,38 +33,43 @@ class DeepSeekMessage(typing.TypedDict):
     tool_call_id: typing.NotRequired[str]
 
 
+class DeepSeekRequestThinking(typing.TypedDict):
+    type: typing.Required[DeepSeekThinkingType]
+
+
 class DeepSeekRequest(typing.TypedDict):
-    model: typing.Required[ModelType]
-    messages: list[DeepSeekMessage]
+    model: typing.Required[DeepSeekModelType]
+    messages: typing.Required[list[DeepSeekRequestMessage]]
+    thinking: typing.Required[DeepSeekRequestThinking]
+    reasoning_effort: typing.NotRequired[DeepSeekReasoningEffortType]
     max_tokens: typing.Required[int]
     stream: typing.Required[bool]
-    temperature: typing.Required[float]
-    tools: typing.NotRequired[list[typing.Dict[str, typing.Any]]]
-    tool_choice: typing.NotRequired[str]
+    tools: typing.Required[list[typing.Dict[str, typing.Any]]]
+    tool_choice: typing.Required[str]
 
 
 API_KEY: str = ""
 BASE_URL: str = "https://api.deepseek.com"
-
-MODEL: ModelType = "deepseek-chat"
-MAX_TOKENS: int = 8192
-
-# MODEL: ModelType = "deepseek-reasoner"
-# MAX_TOKENS: int = 65536
-
-# TEMPERATURE: float = 0.0
-TEMPERATURE: float = 1.0
-# TEMPERATURE: float = 1.3
-# TEMPERATURE: float = 1.5
-
+MAX_TOKENS: int = 393216
 TOOL_CHOICE: ToolChoiceType = "auto"
+STREAM: bool = False
+WAIT_AFTER_ERROR: int = 2
+
+MODEL: DeepSeekModelType = "deepseek-v4-flash"
+# MODEL: DeepSeekModelType = "deepseek-v4-pro"
+
+THINKING: DeepSeekThinkingType = "disabled"
+# THINKING: DeepSeekThinkingType = "enabled"
+
+REASONING_EFFORT: DeepSeekReasoningEffortType = "high"
+# REASONING_EFFORT: DeepSeekReasoningEffortType = "max"
 
 TOOLS: list[typing.Dict[str, typing.Any]] = [
     {
         "type": "function",
         "function": {
-            "name": "run_bash",
-            "description": "Run any bash command",
+            "name": "run_bash_command",
+            "description": "Run any bash command on the user's system",
             "parameters": {
                 "type": "object",
                 "properties": {"command": {"type": "string", "description": "The bash command to run"}},
@@ -68,7 +77,23 @@ TOOLS: list[typing.Dict[str, typing.Any]] = [
                 "additionalProperties": False,
             },
         },
-    }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_random_integer",
+            "description": "Return a random integer between min and max (inclusive)",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "min": {"type": "integer", "description": "The minimum integer (inclusive)"},
+                    "max": {"type": "integer", "description": "The maximum integer (inclusive)"},
+                },
+                "required": ["min", "max"],
+                "additionalProperties": False,
+            },
+        },
+    },
 ]
 
 
@@ -91,44 +116,78 @@ SYSTEM_COMMAND_STDOUT_4, SYSTEM_COMMAND_STDERR_4, SYSTEM_COMMAND_RETURNCODE_4 = 
 
 
 def get_formatted_command_output(stdout: str, stderr: str, returncode: int) -> str:
-    return f"------- STDOUT -------\n\n{stdout.strip()}\n\n------- STDERR -------\n\n{stderr.strip()}\n\n----- RETURNCODE -----\n\n{returncode}"
+    formatted_command_output: str = ""
+    trimmed_stdout = stdout.strip()
+    if len(trimmed_stdout) != 0:
+        formatted_command_output += f"--- STDOUT ---\n\n{trimmed_stdout}\n\n"
+    trimmed_stderr = stderr.strip()
+    if len(trimmed_stderr) != 0:
+        formatted_command_output += f"--- STDERR ---\n\n{trimmed_stderr}\n\n"
+    if returncode != 0:
+        formatted_command_output += f"--- RETURNCODE ---\n\n{returncode}\n\n"
+    return formatted_command_output.strip()
 
 
 SYSTEM_MESSAGES: list[str] = [
+    "You must always reply using proper English (US) grammar",
+    "You must always reply using strict Markdown syntax with proper formatting",
+    "You are an AI assistant operating in a text-only terminal interface",
+    'You are a capable of running any bash commands on the user\'s system using the "run_bash_command" function',
+    'You are capable of getting a random integer number using the "get_random_integer" function',
     f"$ {SYSTEM_COMMAND_1}\n\n{get_formatted_command_output(SYSTEM_COMMAND_STDOUT_1, SYSTEM_COMMAND_STDERR_1, SYSTEM_COMMAND_RETURNCODE_1)}",
     f"$ {SYSTEM_COMMAND_2}\n\n{get_formatted_command_output(SYSTEM_COMMAND_STDOUT_2, SYSTEM_COMMAND_STDERR_2, SYSTEM_COMMAND_RETURNCODE_2)}",
     f"$ {SYSTEM_COMMAND_3}\n\n{get_formatted_command_output(SYSTEM_COMMAND_STDOUT_3, SYSTEM_COMMAND_STDERR_3, SYSTEM_COMMAND_RETURNCODE_3)}",
     f"$ {SYSTEM_COMMAND_4}\n\n{get_formatted_command_output(SYSTEM_COMMAND_STDOUT_4, SYSTEM_COMMAND_STDERR_4, SYSTEM_COMMAND_RETURNCODE_4)}",
-    "You are a terminal-based AI assistant capable of running any bash commands on the user's system.",
 ]
 
 
 def get_llm_output(
     llm_base_url: str,
     llm_api_key: str,
-    llm_model: ModelType,
-    llm_messages: list[DeepSeekMessage],
+    llm_model: DeepSeekModelType,
+    llm_messages: list[DeepSeekRequestMessage],
+    llm_thinking: DeepSeekThinkingType,
+    llm_reasoning_effort: DeepSeekReasoningEffortType,
     llm_max_tokens: int,
-    llm_temperature: float,
     llm_tools: list[typing.Dict[str, typing.Any]],
     llm_tool_choice: ToolChoiceType,
+    llm_stream: bool,
+    llm_wait_after_error: int,
 ) -> tuple[str, str, list[ToolCall]]:
     headers: typing.Mapping[str, str] = {"Authorization": f"Bearer {llm_api_key}", "Content-Type": "application/json"}
+    payload_thinking: DeepSeekRequestThinking = {"type": llm_thinking}
     payload: DeepSeekRequest = {
         "model": llm_model,
         "messages": llm_messages,
+        "thinking": payload_thinking,
         "max_tokens": llm_max_tokens,
-        "stream": False,
-        "temperature": llm_temperature,
+        "stream": llm_stream,
         "tools": llm_tools,
         "tool_choice": llm_tool_choice,
     }
+    if llm_thinking == "enabled":
+        payload["reasoning_effort"] = llm_reasoning_effort
     response = requests.post(f"{llm_base_url}/chat/completions", headers=headers, json=payload)
-    data = response.json()
     if response.status_code != 200:
+        if response.status_code >= 500 and response.status_code <= 599:
+            time.sleep(llm_wait_after_error)
+            return get_llm_output(
+                llm_base_url,
+                llm_api_key,
+                llm_model,
+                llm_messages,
+                llm_thinking,
+                llm_reasoning_effort,
+                llm_max_tokens,
+                llm_tools,
+                llm_tool_choice,
+                llm_stream,
+                llm_wait_after_error,
+            )
         print(json.dumps(payload, indent=2))
         print(response.status_code)
-        print(json.dumps(data, indent=2))
+        print(json.dumps(response.json(), indent=2))
+    data = response.json()
     message = data["choices"][0]["message"]
     content: str = message.get("content", "").strip()
     reasoning_content: str = message.get("reasoning_content", "").strip()
@@ -148,7 +207,7 @@ def get_llm_output(
 
 
 def add_to_llm_messages(
-    llm_messages: list[DeepSeekMessage],
+    llm_messages: list[DeepSeekRequestMessage],
     role: RoleType,
     content: str,
     reasoning_content: str = "",
@@ -157,42 +216,56 @@ def add_to_llm_messages(
 ) -> None:
     trimmed_content: str = content.strip()
     trimmed_reasoning_content: str = reasoning_content.strip()
-    if role in ["system", "user"]:
-        new_message: DeepSeekMessage = {"role": role, "content": trimmed_content}
-        llm_messages.append(new_message)
-    elif role == "assistant":
-        new_assistant_message: DeepSeekMessage = {"role": role, "content": trimmed_content}
+    if role in ["assistant", "system", "user"]:
+        new_assistant_message: DeepSeekRequestMessage = {"role": role, "content": trimmed_content}
         if len(trimmed_reasoning_content) != 0:
             new_assistant_message["reasoning_content"] = trimmed_reasoning_content
         if len(tool_calls) != 0:
             new_assistant_message["tool_calls"] = tool_calls
         llm_messages.append(new_assistant_message)
     elif role == "tool":
-        new_tool_message: DeepSeekMessage = {"role": role, "content": trimmed_content, "tool_call_id": tool_call_id}
+        new_tool_message: DeepSeekRequestMessage = {
+            "role": role,
+            "content": trimmed_content,
+            "tool_call_id": tool_call_id,
+        }
         llm_messages.append(new_tool_message)
 
 
-def print_command(command: str) -> None:
+def print_bash_command(command: str) -> None:
     input(
-        f"------------------------------------- TOOL -------------------------------------\n\n{command}\n\nPress ENTER to continue..."
+        f"------------------------------------- TOOL -------------------------------------\n{command}\n\nPress ENTER to continue..."
     )
-    print("\n", end="")
 
 
-def process_tool_calls(tool_calls: list[ToolCall], messages: list[DeepSeekMessage]) -> None:
+def print_random_integer(min_integer: int, max_integer: int) -> None:
+    print(
+        f'------------------------------------- TOOL -------------------------------------\nPicking a random integer between "{min_integer}" (inclusive) and "{max_integer}" (inclusive)\n',
+        end="",
+    )
+
+
+def process_tool_calls(tool_calls: list[ToolCall], messages: list[DeepSeekRequestMessage]) -> None:
     for tool_call in tool_calls:
-        if tool_call["function"]["name"] == "run_bash":
+        if tool_call["function"]["name"] == "run_bash_command":
             command: str = ""
-            args = json.loads(tool_call["function"]["arguments"])
-            command = args["command"]
-            print_command(command)
+            function_arguments = json.loads(tool_call["function"]["arguments"])
+            command = function_arguments["command"]
+            print_bash_command(command)
             stdout, stderr, returncode = execute_bash_command(command)
             formatted_command_output = get_formatted_command_output(stdout, stderr, returncode)
             add_to_llm_messages(messages, "tool", formatted_command_output, "", [], tool_call["id"])
+        elif tool_call["function"]["name"] == "get_random_integer":
+            function_arguments = json.loads(tool_call["function"]["arguments"])
+            min_integer: int = function_arguments["min"]
+            max_integer: int = function_arguments["max"]
+            print_random_integer(min_integer, max_integer)
+            random_integer = random.randint(min_integer, max_integer)
+            add_to_llm_messages(messages, "tool", str(random_integer), "", [], tool_call["id"])
 
 
-def create_llm_messages(llm_system_messages: list[str]) -> list[DeepSeekMessage]:
-    llm_messages: list[DeepSeekMessage] = []
+def create_llm_messages(llm_system_messages: list[str]) -> list[DeepSeekRequestMessage]:
+    llm_messages: list[DeepSeekRequestMessage] = []
     for llm_system_message in llm_system_messages:
         trimmed_system_message: str = llm_system_message.strip()
         if len(trimmed_system_message) != 0:
@@ -203,30 +276,39 @@ def create_llm_messages(llm_system_messages: list[str]) -> list[DeepSeekMessage]
 def print_message(message: str, reasoning: str = "") -> None:
     rich_console_instance = rich_console.Console(no_color=True)
     if len(reasoning) != 0:
-        print(f"---------------------------- ASSISTANT (reasoning) -----------------------------\n\n", end="")
+        print(f"---------------------------- ASSISTANT (reasoning) -----------------------------\n", end="")
         rich_console_instance.print(rich_markdown.Markdown(reasoning))
-        print(f"\n", end="")
     if len(message) != 0:
-        print(f"---------------------------------- ASSISTANT -----------------------------------\n\n", end="")
+        print(f"---------------------------------- ASSISTANT -----------------------------------\n", end="")
         rich_console_instance.print(rich_markdown.Markdown(message))
-        print(f"\n", end="")
 
 
 def main() -> None:
-    print("\n", end="")
     llm_messages = create_llm_messages(SYSTEM_MESSAGES)
     try:
         while True:
             llm_input = input(
-                "------------------------------------- USER -------------------------------------\n\n> "
+                "------------------------------------- USER -------------------------------------\n> "
             ).strip()
-            print("\n", end="")
             if len(llm_input) == 0:
                 continue
-            add_to_llm_messages(llm_messages, "user", llm_input)
+            if llm_input == "/rewind":
+                del llm_messages[-1]
+            else:
+                add_to_llm_messages(llm_messages, "user", llm_input)
             while True:
                 llm_output, llm_output_reasoning, tool_calls = get_llm_output(
-                    BASE_URL, API_KEY, MODEL, llm_messages, MAX_TOKENS, TEMPERATURE, TOOLS, TOOL_CHOICE
+                    BASE_URL,
+                    API_KEY,
+                    MODEL,
+                    llm_messages,
+                    THINKING,
+                    REASONING_EFFORT,
+                    MAX_TOKENS,
+                    TOOLS,
+                    TOOL_CHOICE,
+                    STREAM,
+                    WAIT_AFTER_ERROR,
                 )
                 print_message(llm_output, llm_output_reasoning)
                 add_to_llm_messages(llm_messages, "assistant", llm_output, llm_output_reasoning, tool_calls)
@@ -235,7 +317,7 @@ def main() -> None:
                 else:
                     break
     except KeyboardInterrupt:
-        print("\n\n--------------------------------------------------------------------------------\n\n", end="")
+        print("\n--------------------------------------------------------------------------------\n", end="")
 
 
 if __name__ == "__main__":
